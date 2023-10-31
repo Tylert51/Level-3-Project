0a9ce66846095523b9eb0247c795da34aa03bdda
tyler <ttang7910@bths.edu>
2023-10-30 22:56:37 -0400

    webdev project 3

---
+++ script.js
@@ -0,0 +1,22 @@
+let table, 
+tileClickedIndex, score;
+
+
+
+function initialize() {
+	
+    table = document.getElementById("sliderTable");
+    tileClickedIndex = null;
+    score = 0;		
+    
+}
+
+function initializeTable()
+{
+    
+}
+
+function start()
+{
+    
+}
